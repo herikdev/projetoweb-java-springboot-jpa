9ed214267b9364a25dcef8ee4b0665481810dc69
herikdev <herikrb.dev@gmail.com>
2025-09-11 14:48:10 -0300

    H2 database, test profile, JPA

---
+++ src/main/java/com/example/projetowebjava/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.example.projetowebjava.resources;
+
+import com.example.projetowebjava.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findOne() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+
+}
---
+++ src/test/java/com/example/projetowebjava/ProjetowebjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetowebjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
